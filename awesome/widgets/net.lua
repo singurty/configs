fd0a7f44c16277b6d444a6611757c9c03653a836
singurty <singurty@gmail.com>
2021-10-21 18:42:34 +0545

    fix net widget

--- awesome/widgets/net.lua
+++ awesome/widgets/net.lua
@@ -17,7 +17,7 @@ local string  = string
 local function factory(args)
     args             = args or {}
 
-    local net        = { widget = args.widget or wibox.widget.textbox(), devices = {} }
+    local net        = { widget = args.widget or wibox.widget.textbox() }
     local timeout    = args.timeout or 2
     local units      = args.units or 1024 -- KB
     local notify     = args.notify or "on"
@@ -42,7 +42,6 @@ local function factory(args)
     function net.update()
         -- These are the totals over all specified interfaces
         net_now = {
-            devices  = {},
             -- Bytes since last iteration
             sent     = 0,
             received = 0
@@ -50,24 +49,10 @@ local function factory(args)
 
         for _, dev in ipairs(net.iface) do
             local dev_now    = {}
-            local dev_before = net.devices[dev] or { last_t = 0, last_r = 0 }
-            local now_t      = tonumber(helpers.first_line(string.format("/sys/class/net/%s/statistics/tx_bytes", dev)) or 0)
-            local now_r      = tonumber(helpers.first_line(string.format("/sys/class/net/%s/statistics/rx_bytes", dev)) or 0)
+            local dev_before = { last_t = 0, last_r = 0 }
 
             dev_now.carrier  = helpers.first_line(string.format("/sys/class/net/%s/carrier", dev)) or "0"
             dev_now.state    = helpers.first_line(string.format("/sys/class/net/%s/operstate", dev)) or "down"
-
-            dev_now.sent     = (now_t - dev_before.last_t) / timeout / units
-            dev_now.received = (now_r - dev_before.last_r) / timeout / units
-
-            net_now.sent     = net_now.sent + dev_now.sent
-            net_now.received = net_now.received + dev_now.received
-
-            dev_now.sent     = string.format("%.1f", dev_now.sent)
-            dev_now.received = string.format("%.1f", dev_now.received)
-
-            dev_now.last_t   = now_t
-            dev_now.last_r   = now_r
 
             if wifi_state == "on" and helpers.first_line(string.format("/sys/class/net/%s/uevent", dev)) == "DEVTYPE=wlan" then
                 dev_now.wifi   = true
@@ -84,7 +69,6 @@ local function factory(args)
                 dev_now.ethernet = false
             end
 
-            net.devices[dev] = dev_now
 
             -- Notify only once when connection is lost
             if string.match(dev_now.carrier, "0") and notify == "on" and helpers.get_map(dev) then
@@ -101,13 +85,9 @@ local function factory(args)
 
             net_now.carrier = dev_now.carrier
             net_now.state = dev_now.state
-            net_now.devices[dev] = dev_now
             -- net_now.sent and net_now.received will be
             -- the totals across all specified devices
         end
-
-        net_now.sent = string.format("%.1f", net_now.sent)
-        net_now.received = string.format("%.1f", net_now.received)
 
         widget = net.widget
         settings()
