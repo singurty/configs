8e51096736d3844653adaa55eeca9cdee4650511
singurty <singurty@gmail.com>
2021-11-03 10:25:46 +0545

    update configs

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -463,6 +463,15 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+    c:emit_signal("request::activate", "mouse_enter", {raise = false})
+end)
+
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+-- }}}
+
 -- Autostart appplications
 awful.spawn.with_shell("nitrogen --restore")
 awful.spawn.with_shell("picom --config=$HOME/.config/picom/picom.conf")
